a523fcd1a763b61043426b994816040e4d625aeb
Thiago Culau <thiago.culau1@outlook.com>
2024-05-21 11:00:00 -0300

    Add project files.

---
+++ eAgenda.WinApp/TelaInicialForm.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace eAgenda.WinApp
+{
+    partial class TelaInicialForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // TelaInicialForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "TelaInicialForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            Load += TelaInicialForm_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
